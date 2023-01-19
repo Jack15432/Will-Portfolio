de190997ff707c845cff5ca9ecbb2eeb4dea2aac
William <k124215545k@gmail.com>
2023-01-19 11:53:51 -0800

    first commit

---
+++ src/components/footer/Footer.jsx
@@ -0,0 +1,63 @@
+import React from "react";
+import "./footer.css";
+
+const Footer = () => {
+  return (
+    <footer className="footer">
+      <div className="footer__container container">
+        <h1 className="footer__title">Smith</h1>
+
+        <ul className="footer__list">
+          <li>
+            <a href="#about" className="footer__link">
+              About
+            </a>
+          </li>
+
+          <li>
+            <a href="#portfolio" className="footer__link">
+              Projects
+            </a>
+          </li>
+
+          <li>
+            <a href="#testimonials" className="footer__link">
+              Testimonials
+            </a>
+          </li>
+        </ul>
+
+        <div className="footer__social">
+          <a
+            href="https://www.instagram.com/willia.m.m.m/"
+            className="footer__social-link"
+            rel="noreferrer"
+            target="_blank"
+          >
+            <i class="bx bxl-instagram"></i>
+          </a>
+          <a
+            href="https://www.facebook.com/profile.php?id=100009259166184"
+            className="footer__social-link"
+            rel="noreferrer"
+            target="_blank"
+          >
+            <i class="bx bxl-facebook"></i>
+          </a>
+          <a
+            href="https://github.com"
+            className="footer__social-link"
+            rel="noreferrer"
+            target="_blank"
+          >
+            <i class="bx bxl-github"></i>
+          </a>
+        </div>
+        <span className="footer__copy">&#169; Crypticalcoder. All rights reserved
+        </span>
+      </div>
+    </footer>
+  );
+};
+
+export default Footer;
---
+++ src/components/home/Social.jsx
@@ -0,0 +1,34 @@
+import React from "react";
+
+const Social = () => {
+  return (
+    <div className="home__social">
+      <a
+        href="https://www.instagram.com/willia.m.m.m/"
+        className="home__social-icon"
+        rel="noreferrer"
+        target="_blank"
+      >
+        <i class="uil uil-instagram"></i>
+      </a>
+      <a
+        href="https://www.facebook.com/profile.php?id=100009259166184"
+        className="home__social-icon"
+        rel="noreferrer"
+        target="_blank"
+      >
+        <i class="uil uil-facebook"></i>
+      </a>
+      <a
+        href="https://github.com"
+        className="home__social-icon"
+        rel="noreferrer"
+        target="_blank"
+      >
+        <i class="uil uil-github"></i>
+      </a>
+    </div>
+  );
+};
+
+export default Social;
